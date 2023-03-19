2542bd3082d95eded0f25a8c5a7d1755b3a25220
Joshua Lochner <admin@xenova.com>
2023-03-19 18:18:27 +0200

    Create basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { pipeline } from '@xenova/transformers';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [predict, setPredict] = useState<Promise<any> | null>(null);
+  const [inputText, setInputText] = useState('');
+  const [prediction, setPrediction] = useState('');
+
+  const loadModel = async () => {
+    const model = await pipeline('text-classification');
+    return model;
+  }
+
+  const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+
+    let model = await predict;
+    if (!model) {
+      model = loadModel();
+      setPredict(model);
+      model = await model;
+    }
+
+    setPrediction(await model(event.target.value))
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <label htmlFor="input-text">Input Text:</label>
+      <input id="input-text" type="text" value={inputText} onChange={handleInputChange} />
+      {prediction && (
+        <div>
+          <h2>Prediction:</h2>
+          <p>{JSON.stringify(prediction)}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
