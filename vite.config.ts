4f858ec50c0df34413ee5cb86da26801152f4200
Joshua Lochner <admin@xenova.com>
2023-03-19 18:11:08 +0200

    Add commonjs plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    commonjs(),
+    react()
+  ],
 })
