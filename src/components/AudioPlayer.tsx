b6984273a9a03447efbb83a2713268edb4bb4b20
Joshua Lochner <admin@xenova.com>
2023-05-29 17:07:47 +0200

    Fix audio source on mobile

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,44 +1,23 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
-enum Status {
-    playing = "PLAYING",
-    paused = "PAUSED",
-    initialised = "INIT",
-    ended = "ENDED",
-    reset = "RESET",
-}
 
 export default function AudioPlayer(props: { audioUrl: string }) {
-    const [status, setStatus] = useState(Status.initialised);
 
     const audioPlayer = useRef<HTMLAudioElement>(null);
+    const audioSource = useRef<HTMLSourceElement>(null);
 
     // Updates src when url changes
     useEffect(() => {
-        if (audioPlayer.current) {
-            audioPlayer.current.src = props.audioUrl;
+        if (audioPlayer.current && audioSource.current) {
+            audioSource.current.src = props.audioUrl;
             audioPlayer.current.load();
         }
-        setStatus(Status.initialised);
     }, [props.audioUrl]);
-
-    // Updates current time (ontimeupdate event is not frequent enough)
-    useEffect(() => {
-        if (audioPlayer.current === null) return;
-
-        let c = audioPlayer.current;
-
-        // Reset player at end of clip
-        if (status === Status.ended) {
-            c.currentTime = 0;
-            setStatus(Status.reset);
-            return;
-        }
-    }, [status]);
 
     return (
         <div className='flex relative z-10 p-4 w-full'>
             <audio ref={audioPlayer} controls className="w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10">
+                <source ref={audioSource} type="audio/wav"></source>
             </audio>
         </div>
     );
