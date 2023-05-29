c1aea6d4f9b055bab6ae0660a9882041fa722b6c
Joshua Lochner <admin@xenova.com>
2023-05-29 13:39:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from '@rollup/plugin-commonjs';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    commonjs(),
     react()
   ],
 })
