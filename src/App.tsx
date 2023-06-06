92f2ba02fef3d172e6db042e0f176738898c66bb
Joshua Lochner <admin@xenova.com>
2023-06-06 16:35:59 +0200

    Center-align heading

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
-                <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl'>
+                <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
                     Whisper Web
                 </h1>
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
