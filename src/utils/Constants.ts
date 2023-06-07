5c5a73b7f4344483929ccd6680d5ce349406f7f8
Joshua Lochner <admin@xenova.com>
2023-06-07 02:11:23 +0200

    Update Constants.ts

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,10 +1,10 @@
 export default {
     SAMPLING_RATE: 16000,
     DEFAULT_AUDIO_URL:
-        "https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/jfk.wav",
+        "https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/ted_60.wav",
     DEFAULT_MODEL: "tiny",
     DEFAULT_SUBTASK: "transcribe",
     DEFAULT_LANGUAGE: "auto",
-    DEFAULT_QUANTIZED: true,
+    DEFAULT_QUANTIZED: false,
     DEFAULT_MULTILINGUAL: false,
 };
