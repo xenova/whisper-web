11691473098a7e729c37d5c993975478e7c154a6
Joshua Lochner <admin@xenova.com>
2023-06-13 17:42:24 +0200

    Add "Export TXT" option

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -10,20 +10,30 @@ interface Props {
 export default function Transcript({ transcribedData }: Props) {
     const divRef = useRef<HTMLDivElement>(null);
 
-    const handleButtonClick = () => {
-        let jsonData = JSON.stringify(transcribedData?.chunks ?? {}, null, 2);
+    const saveBlob = (blob: Blob, filename: string) => {
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = filename;
+        link.click();
+        URL.revokeObjectURL(url);
+    };
+    const exportTXT = () => {
+        let chunks = transcribedData?.chunks ?? [];
+        let text = chunks.map((chunk) => chunk.text).join('').trim();
+
+        const blob = new Blob([text], { type: "text/plain" });
+        saveBlob(blob, "transcript.txt");
+    }
+    const exportJSON = () => {
+        let jsonData = JSON.stringify(transcribedData?.chunks ?? [], null, 2);
 
         // post-process the JSON to make it more readable
         const regex = /(    "timestamp": )\[\s+(\S+)\s+(\S+)\s+\]/gm;
         jsonData = jsonData.replace(regex, "$1[$2 $3]");
 
         const blob = new Blob([jsonData], { type: "application/json" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "transcript.json";
-        link.click();
-        URL.revokeObjectURL(url);
+        saveBlob(blob, "transcript.json");
     };
 
     // Scroll to the bottom when the component updates
@@ -31,8 +41,8 @@ export default function Transcript({ transcribedData }: Props) {
         if (divRef.current) {
             const diff = Math.abs(
                 divRef.current.offsetHeight +
-                    divRef.current.scrollTop -
-                    divRef.current.scrollHeight,
+                divRef.current.scrollTop -
+                divRef.current.scrollHeight,
             );
 
             if (diff <= 64) {
@@ -62,7 +72,13 @@ export default function Transcript({ transcribedData }: Props) {
             {transcribedData && !transcribedData.isBusy && (
                 <div className='w-full text-right'>
                     <button
-                        onClick={handleButtonClick}
+                        onClick={exportTXT}
+                        className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
+                    >
+                        Export TXT
+                    </button>
+                    <button
+                        onClick={exportJSON}
                         className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
                     >
                         Export JSON
