2869a7daba329b06c8bf1e77506b90eea42cef84
Pushpender Saini <54404738+PushpenderSaini0@users.noreply.github.com>
2023-06-22 16:50:11 +0530

    Add SRT export

--- src/utils/AudioUtils.ts
+++ src/utils/AudioUtils.ts
@@ -1,14 +1,51 @@
-function padTime(time: number) {
-    return String(time).padStart(2, "0");
+const HOURS_IN_SECONDS = 3600;
+const MINUTES_IN_SECONDS = 60;
+
+function padTime(time: number, padding: number = 2) {
+    return String(time).padStart(padding, "0");
+}
+
+function formatTimestamp(time: number) {
+
+    const hours = Math.floor(time / HOURS_IN_SECONDS);
+    const hoursRemainder = time - hours * HOURS_IN_SECONDS;
+
+    const minutes = Math.floor(hoursRemainder / MINUTES_IN_SECONDS);
+    const minutesRemainder = hoursRemainder - minutes * MINUTES_IN_SECONDS;
+
+    const seconds = Math.floor(minutesRemainder);
+    const secondsRemainder = minutesRemainder - seconds;
+
+    const milliseconds = Math.floor(secondsRemainder * 1000);
+
+    return { hours, minutes, seconds, milliseconds };
 }
 
 export function formatAudioTimestamp(time: number) {
-    const hours = (time / (60 * 60)) | 0;
-    time -= hours * (60 * 60);
-    const minutes = (time / 60) | 0;
-    time -= minutes * 60;
-    const seconds = time | 0;
-    return `${hours ? padTime(hours) + ":" : ""}${padTime(minutes)}:${padTime(
-        seconds,
-    )}`;
+
+    const { hours, minutes, seconds } = formatTimestamp(time);
+
+    // Hide hours if not needed
+    const hoursString = hours ? padTime(hours) + ":" : "";
+    const minutesString = padTime(minutes) + ":";
+    const secondsString = padTime(seconds);
+
+    return `${hoursString}${minutesString}${secondsString}`;
+}
+
+function formatSrtTimestamp(time: number) {
+
+    const { hours, minutes, seconds, milliseconds } = formatTimestamp(time);
+
+    const hoursString = padTime(hours) + ":";
+    const minutesString = padTime(minutes) + ":";
+    const secondsString = padTime(seconds) + ",";
+    const millisecondsString = padTime(milliseconds, 3);
+
+    return `${hoursString}${minutesString}${secondsString}${millisecondsString}`;
+}
+
+
+export function formatSrtTimeRange(start: number, end: number) {
+    return `${formatSrtTimestamp(start)} --> ${formatSrtTimestamp(end)}`;
 }
